6a81b60e3438eb53f32474167b044983329401f5
Inna Shcherbakova <ryabinna1986@mail.ru>
2023-04-29 18:36:12 +0300

    new deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
  
-git push -f git@github.com:ryabinna/headphones.git master:gh-pages
+git push -f git@github.com:ryabinna/headphones.git main:gh-pages
 
 cd -
